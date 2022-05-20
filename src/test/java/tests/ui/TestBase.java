1f58f342617ece43dda5a526a97cd2fa52b06acc
Umthekingofthegreatescape <abdelaziznassif94@gmail.com>
2022-05-20 16:46:41 +0200

    adding github actions

--- src/test/java/tests/ui/TestBase.java
+++ src/test/java/tests/ui/TestBase.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import pages.login.LoginPage;
@@ -17,8 +18,12 @@ public class TestBase extends PropertiesFileReader {
     @BeforeClass
     public void setup ()
     {
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("disable-infobars") ;
+        var headless = Boolean.parseBoolean(System.getenv("HEADLESS_CHROME")) | false ;
+        chromeOptions.setHeadless(headless) ;
         WebDriverManager.chromedriver().setup() ;
-        driver = new ChromeDriver() ;
+        driver = new ChromeDriver(chromeOptions) ;
         Dimension desiredBrowserDimensions = new Dimension(1024,768);
         loadUIStaticConfigs("ui_config.properties");
         driver.manage().window().setSize(desiredBrowserDimensions);
